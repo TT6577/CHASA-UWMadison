a18269cb7ababb52dfff22e1291d5a3457be02d5
TT6577 <tylerzt28@gmail.com>
2026-06-04 23:57:19 -0500

    footer & about page updates, adding vercel.json to fix url issues

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,11 @@ import Fams from "./pages/Fams.jsx";
 import History from "./pages/History.jsx";
 import Interns from "./pages/Interns.jsx";
 
+// Icons
 import { SquareMenu, SquareX } from "lucide-react";
+import { AiFillInstagram } from "react-icons/ai";
+import { SiGroupme } from "react-icons/si";
+import { IoMail } from "react-icons/io5";
 
 // Motion
 import { AnimatePresence, motion } from "framer-motion";
@@ -271,20 +275,47 @@ function App() {
             <div style={{ height: "50px" }}></div>
             <div id="footer">
               <div style={{ height: "30px" }}></div>
-              <p
-                className="footer-text"
-                style={{ width: "60%", margin: "0 auto" }}
-              >
-                Built using React, JavaScript and CSS. Hosted on Vercel. Full
-                website code{" "}
-                <a
-                  className="bright-link"
-                  href="https://github.com/TT6577/CHASA-UWMadison"
-                  target="_blank"
+              <div style={{ display: "flex", justifyContent: "space-between", alignItems: 'center' }}>
+                <div
+                  className="flex-side"
+                  style={{ marginLeft: "3rem", alignItems: "center" }}
                 >
-                  here.
-                </a>
-              </p>
+                  <a
+                    className="footer-text"
+                    href="mailto:chasauwmadison@gmail.com"
+                    target="_blank"
+                  >
+                    <IoMail size="55px" />
+                  </a>
+                  <a
+                    className="footer-text"
+                    href="https://www.instagram.com/chasamadison"
+                    target="_blank"
+                  >
+                    <AiFillInstagram size="50px" />
+                  </a>
+                  <a
+                    className="footer-text"
+                    href="https://groupme.com/join_group/106007294/OPtpknel"
+                    target="_blank"
+                  >
+                    <SiGroupme size="40px" />
+                  </a>
+                </div>
+
+                <p className="footer-text" style={{ width: "60%" }}>
+                  Built using React, JavaScript and CSS. Hosted on Vercel. Full
+                  website code{" "}
+                  <a
+                    className="bright-link"
+                    href="https://github.com/TT6577/CHASA-UWMadison"
+                    target="_blank"
+                  >
+                    here.
+                  </a>
+                </p>
+              </div>
+
               <div style={{ height: "50px" }}></div>
             </div>
           </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,8 +11,22 @@ export default function About() {
       transition={{ duration: 0.3 }}
     >
       <div style={{ height: "100px" }}></div>
-      <div className="green-background-div">
+      <div className="green-background-div flex-down">
         <h1>About CHASA</h1>
+        <h2>Mission Statement:</h2>
+        <p className="footer-text" style={{margin: '0 5rem 0 5rem'}}>
+          Chinese American Student Association is designed for those on campus
+          who are interested in Chinese American culture. Our goal is to provide
+          a space where students can build community amongst their peers and be
+          able to learn more about their own individual identities.
+        </p>
+        <p className="footer-text" style={{margin: '0 5rem 0 5rem'}}>
+          We recognize the importance of acknowledging the two different
+          cultures and how having two backgrounds can impact one’s college
+          experience. Through social events and community outreach, CHASA
+          strives to establish a better understanding of Chinese American
+          culture at UW-Madison.
+        </p>
       </div>
     </motion.div>
   );
